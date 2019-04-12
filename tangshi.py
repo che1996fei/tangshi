f6082f8ed1a6e5bd6aa812b320b07b689bd8caf3
chefei <48946109+che1996fei@users.noreply.github.com>
2019-04-12 15:47:14 +0800

    Update tangshi.py

--- tangshi.py
+++ tangshi.py
@@ -1,8 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 from requests.exceptions import RequestException
-from pyquery import PyQuery as pq
+
 
 def get_page():
     url = 'https://www.gushiwen.org/gushi/tangshi.aspx'
@@ -34,24 +33,46 @@ def get_detail_page(url):
         return None
 
 def parse_detail_page(html):
-    pattern = re.compile('<h1 style=".*?">(.*?)</h1>', re.S)
-    pattern1 = re.compile('<p class="source"><a href=.*?>(.*?)</a><span>(.*?)</span><a href=.*?>(.*?)</a>', re.S)
-    pattern2 = re.compile('<div class="contson" id=.*?>(.*?)</div>', re.S)
-    data = re.findall(pattern, html)
-    data1 = re.findall(pattern1, html)[0]
-    data2 = re.findall(pattern2, html)[0]
-    data_ = "".join(data)
-    data1_= "".join(data1)
-    data2_ = str(data2).replace('<br />','\n')
-    print(data_,'\n', data1_, data2_)
+    data = re.findall('<h1 style=".*?">(.*?)</h1>', html, re.S)
+    data1 = re.findall('<p class="source"><a href=.*?>(.*?)</a><span>(.*?)</span><a href=.*?>(.*?)</a>', html, re.S)
+    if data1:
+        data1 = data1[0]
+    else:
+        data1 = ""
+    data2 = re.findall('<div class="contson" id=.*?>(.*?)</div>', html, re.S)
+    if data2:
+        data2 = data2[0]
+    else:
+        data2 = ""
+    data3 = re.findall('<p><strong>(.*?)</strong>(.*?)</p>', html, re.S)
+    if data3:
+        data3 = data3[0][1]
+    else:
+        data3 = ""
+    data4 = re.findall('<p style=" margin:0px;">(.*?)<a href=".*?>►.*?</a></p', html, re.S)
+    if data4:
+        data4 = data4[0]
+    else:
+        data4 = ""
+    name = "".join(data)
+    author= "".join(data1)
+    content = str(data2).replace('<br />','\n')
+    translation = data3.replace('<br />', ' ')
+    author_life = "".join(data4)
+    file = open('tangshi.txt', 'a', encoding='utf-8')
+    file.write(name + '\n' + author + content + "译文：" + translation + '\n' + "作者：" + author_life)
+    file.write('\n' + '=' * 50 + '\n')
+    print("输出chengg")
+    file.close()
+
 
 
 def main():
     html = get_page()
     for url in  parse_page(html):
         html = get_detail_page(url)
-        data = parse_detail_page(html)
-        print(data)
+        parse_detail_page(html)
+
 
 
 
